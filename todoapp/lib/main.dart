a97057b12c18b7c36e83a6f4543bd733cf30fc57
ausafgill <ausaf.gill2003@gmail.com>
2023-07-19 08:54:41 -0700

    todoflutter

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todoapp/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
